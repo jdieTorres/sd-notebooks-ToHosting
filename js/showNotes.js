bea125e7413f364a58f8a7cb641fd8959c32d4e4
jdieTorres <105823430+jdieTorres@users.noreply.github.com>
2023-11-18 18:21:49 -0500

    Add files via upload

---
+++ js/showNotes.js
@@ -0,0 +1,133 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getFirestore, collection, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js"
+
+const uid = sessionStorage.getItem('uid');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDdzu88Yc2hFkOBK0BrcvSGX-4yDbAs8R0",
+    authDomain: "sd-notebooks-6e2a8.firebaseapp.com",
+    projectId: "sd-notebooks-6e2a8",
+    storageBucket: "sd-notebooks-6e2a8.appspot.com",
+    messagingSenderId: "58677219226",
+    appId: "1:58677219226:web:457132eb82f664bfa63423"
+};
+
+const app = initializeApp(firebaseConfig);
+export const FirebaseDB = getFirestore(app);
+
+const loadNotes = async (uid = '') => {
+
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+    const docs = await getDocs(collectionRef);
+
+    const notes = [];
+    docs.forEach(doc => {
+        notes.push({ id: doc.id, ...doc.data() });
+    });
+
+    return notes
+
+};
+
+const notes = await loadNotes(uid);
+
+const deleteNote = async (uid, noteId) => {
+    await deleteDoc(doc(collection(FirebaseDB, `${uid}/journal/notes`), noteId));
+    const updatedNotes = await loadNotes(uid);
+    updateNotesList(updatedNotes);
+};
+
+const updateNote = async (uid, noteId, updatedData) => {
+    await updateDoc(doc(collection(FirebaseDB, `${uid}/journal/notes`), noteId), updatedData);
+    const updatedNotes = await loadNotes(uid);
+    updateNotesList(updatedNotes);
+};
+
+const renderNotes = (notes) => {
+    document.getElementById("notesList").innerHTML = "";
+    notes.forEach(note => {
+        const noteItem = document.createElement("li");
+        noteItem.className = "notes-list-item";
+
+        const noteIcon = document.createElement("i");
+        noteIcon.className = "notes-list-item-icon fa-regular fa-bookmark";
+
+        const noteTitle = document.createElement("h4");
+        noteTitle.textContent = note.title;
+
+        const noteBody = document.createElement("p");
+        noteBody.textContent = note.body;
+
+        const noteChars = document.createElement("div");
+        noteChars.className = "chars-grid";
+        noteChars.appendChild(noteTitle);
+        noteChars.appendChild(noteBody);
+
+        const noteDate = document.createElement("p");
+        noteDate.textContent = note.date;
+
+        const deleteButton = document.createElement("i");
+        deleteButton.className = "delete-buttom fa-regular fa-trash-can fa-2xl";
+
+        deleteButton.addEventListener("click", () => {
+            deleteNote(uid, note.id);
+        });
+
+        const updateButton = document.createElement("i");
+        updateButton.className = "update-buttom fa-regular fa-pen-to-square fa-2xl";
+
+        updateButton.addEventListener("click", () => {
+            const modal = document.getElementById("updateModal");
+            const updatedTitleInput = document.getElementById("updatedTitle");
+            const updatedBodyInput = document.getElementById("updatedBody");
+
+            updatedTitleInput.value = note.title;
+            updatedBodyInput.value = note.body;
+
+            modal.style.display = "block";
+
+            const saveChangesButton = document.getElementById("saveChanges");
+            saveChangesButton.addEventListener("click", () => {
+                const updatedTitle = document.getElementById("updatedTitle").value;
+                const updatedBody = document.getElementById("updatedBody").value;
+
+                if (updatedTitle.trim() !== "" && updatedBody.trim() !== "") {
+                    updateNote(uid, note.id, { title: updatedTitle, body: updatedBody });
+                    const modal = document.getElementById("updateModal");
+                    modal.style.display = "none";
+                } else {
+                    alert("Por favor, complete todos los campos.");
+                }
+            });
+        });
+
+
+        noteItem.appendChild(noteIcon);
+        noteItem.appendChild(noteChars);
+        noteItem.appendChild(noteDate);
+        noteItem.appendChild(deleteButton);
+        noteItem.appendChild(updateButton);
+
+        document.getElementById("notesList").appendChild(noteItem);
+    });
+};
+
+const updateNotesList = (updatedNotes) => {
+    renderNotes(updatedNotes);
+};
+
+renderNotes(notes);
+
+const closeModalButton = document.getElementById("closeModal");
+
+closeModalButton.addEventListener("click", () => {
+    const modal = document.getElementById("updateModal");
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    const modal = document.getElementById("updateModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
---
+++ views/notes_view.html
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <script src="../js/createNotes.js" defer type="module"></script>
+  <script src="../js/showNotes.js" defer type="module"></script>
+  <!--<script src="../js/changeViews.js" defer type="module"></script> -->
+  <link rel="stylesheet" type="text/css" href="../styles/styles_notes_view.css">
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet">
+
+  <!-- Icons -->
+  <script src="https://kit.fontawesome.com/a59d4e3777.js" crossorigin="anonymous"></script>
+
+  <script>
+
+    const displayName = sessionStorage.getItem('displayName');
+    const email = sessionStorage.getItem('email');
+    const photoURL = sessionStorage.getItem('photoURL');
+    const uid = sessionStorage.getItem('uid');
+
+  </script>
+
+  <title>Notes View</title>
+
+</head>
+
+<body>
+  <div class="aside-notes">
+    <div class="aside-header">
+      <h2>
+        <script>document.write(displayName)</script>
+      </h2>
+    </div>
+
+    <div class="aside-grid">
+
+      <ul id="notesList">
+
+      </ul>
+
+    </div>
+
+    <div class="aside-setts">
+      <h3>Ajustes</h3>
+    </div>
+
+  </div>
+
+  <div class="main-notes">
+
+    <div class="main-nav">
+      <h2>SD Notebooks</h2>
+
+      <a href="./index.html"><i class="fa-solid fa-up-right-from-square"></i></a>
+
+    </div>
+
+    <!--<div class="no-select-view" id="noNote">
+      <h3>Da Click en el boton "+" para comenzar</h3>
+
+      <button class="new-note-button" id="changeViews">
+        <i class="fa-solid fa-plus"></i>
+      </button>
+
+    </div> -->
+
+    <div class="main-note-form" id="newNote">
+      <h3>
+        <script>document.write(new Date().toDateString())</script>
+      </h3>
+
+      <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" id="newNoteForm">
+        <input type="text" placeholder="Titulo" id="title" required>
+        <textarea name="content" rows="4" cols="50" placeholder="Escribe tu nota!" id="content" required></textarea>
+
+        <button class="new-note-button" type="submit" id="newNoteButton">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</body>
+
+</html>
